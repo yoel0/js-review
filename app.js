43fce3acfa549f79fb2f2aca1a09d30840d3d9c8
yoel <yoel@macbook-pro.lan>
2020-07-20 18:18:04 -0700

    [jshw] solve 5 problems

--- app.js
+++ app.js
@@ -131,8 +131,73 @@ headerThree.addEventListener("click", function () {
 // - homie 1
 // - homie 2
 // - homie 3
+
 // ordered list
 // homies
 // 1. homie 1
 // 2. homie 2
 // 3. homie 3
+
+// Problem Solving
+
+// what do I start with
+// what do I I hope to end up with
+
+// understand the problem
+// where the issue start and ...
+
+// break it down into more manageable problems
+
+// fizzBuzz
+// Write a function that does the following:
+// takes in an array
+// check each number in the array
+// if the number is divisible by (need to = 0) 5 and 3 === FizzBuzz
+// if the number is divisble by 3 === Fizz
+// if the number is divisble by 5 === 'Buzz'
+// return an array
+
+// For example:
+// [3, 5, 15, 20, 9, 7]
+// ['Fizz', 'Buzz', 'FizzBuzz', 'Buzz', 'Fizz', 7]
+
+// I need to set up an empty array
+// iterate through the array thats going to be passed in
+// check the remainder of each element
+// push the result of each element inside the result array
+// return that result array
+
+function fizzBuzz(array) {
+  let result = []; // Fizz, Buzz, FizzBuzz, Buzz, Fizz, 7
+
+  for (let i = 0; i < array.length; i++) {
+    let num = array[i];
+    console.log(num); // 3, 5, 15....
+
+    if (num % 3 === 0 && num % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (num % 3 === 0) {
+      result.push("Fizz");
+    } else if (num % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(num);
+    }
+  }
+
+  return result;
+}
+
+console.log(fizzBuzz([3, 5, 15, 20, 9, 7]));
+
+// - [ ] Do I understand what the prompt is asking?
+// - [ ] Am I able to break the problem down?
+// - [ ] What is my strategy for solving the problem?
+// - [ ] Do I understand what my code is doing?
+// - [ ] Am I able to foresee any edge cases?
+// - [ ] Am I able to track the order of the code that is being executed?
+// - [ ] Am I able to track the values of the variables?
+// - [ ] Am I able to walk through my code line by line with an example?
+// - [ ] Do I have a working solution?
+
+// PairProgramming Problems (Driver, Navigator):
